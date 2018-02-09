5a7cb3e8e5600ebd81873168da7df1c0d7f69592
leo <contemplation8213@gmail.com>
2018-02-09 19:17:18 +0800

    修正路徑

--- test/hero.test.ts
+++ test/hero.test.ts
@@ -9,7 +9,7 @@ const expect = chai.expect;
 
 describe('Get Heho list', () => {
     it('it should be array and length is not zero', ()=>{
-        return chai.request(app).get('/heroes')
+        return chai.request(app).get('/hero')
         .then((res)=>{
             expect(res.status).to.equal(200);
             expect(res).to.be.json;
@@ -19,7 +19,7 @@ describe('Get Heho list', () => {
     });
 
     it('the first one should be leo', ()=>{
-        return chai.request(app).get('/heroes/1')
+        return chai.request(app).get('/hero/1')
         .then((res)=>{
             let hero = res.body;
             expect(res).to.be.json;
@@ -29,7 +29,7 @@ describe('Get Heho list', () => {
     });
 
     it('the sixth hero should not exist', ()=>{
-        return chai.request(app).get('/heroes/6')
+        return chai.request(app).get('/hero/6')
         .then((res)=>{
             expect(res.body).to.equal(null);
         });
